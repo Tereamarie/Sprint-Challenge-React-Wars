305c939841f25c95695eab6f2ac126fb8c728357
marie-hotchkiss <mtknowlton67@gmail.com>
2020-02-09 21:00:07 -0800

    Finished MVP

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,28 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import CharacterList from "./components/CharacterList";
-import styled from "styled-components";
+import axios from 'axios';
+import Characters from './components/Characters';
+import styled from 'styled-components';
 
-
-const Header = styled.h1`
-font-size: 120px;
-margin-top: 10px;
+const StyledContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
 `;
 
-
+const Header = styled.h1`
+text-align: center;
+color: grey;
+`;
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
+  const [characters, setCharacters] = useState([]);
+
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect(() => {
+    axios.get('https://swapi.co/api/people/')
+    .then(response  => {
+      setCharacters(response.data.results)
+    })
+    .catch( err => console.log(err));
+  }, []);
+
   return (
     <div className="App">
-      <Header>React Wars</Header>
-      <CharacterList />
+      <Header className="Header">React Wars</Header>
+      <StyledContainer>
+        {characters.map(characters => <Characters props={characters} />)}
+      </StyledContainer>
     </div>
   );
 }
